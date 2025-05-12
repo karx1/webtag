253ef35751304fefac0411325f323083d51ee05b
Yash Karandikar <nerdstep710@gmail.com>
2025-05-12 18:21:11 -0500

    Full application skeleton

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,34 @@ fn main() {
             Wrapper {
                 Center {
                     h1 { "WebTag" }
-                    p { "Hello, world!" }
+                    label(r#for="song_upload") {"Select an audio file: "}
+                    input(
+                        r#type="file",
+                        id="song_upload",
+                        name="song_upload",
+                        accept="audio/mpeg, audio/wav, audio/aiff, audio/flac, audio/mp4, audio/ogg, audio/opus") {}
+                    div(class="row") {
+                        div(class="column") {
+                            Wrapper {
+                                img(class="thumb", width="150", height="150") {}
+                                input(
+                                    r#type="file",
+                                    id="thumb_upload",
+                                    name="thumb_upload",
+                                    accept="image/jpeg, image/png, image/webp, image/gif") {}
+                            }
+                        }
+                        div(class="column") {
+                            Wrapper {
+                                label(r#for="title") { "Title" }
+                                input(r#type="text", id="title", name="title") {}
+                                label(r#for="artist") { "Artist" }
+                                input(r#type="text", id="artist", name="artist") {}
+                                label(r#for="album") { "Album" }
+                                input(r#type="text", id="album", name="album") {}
+                            }
+                        }
+                    }
                 }
             }
         }
